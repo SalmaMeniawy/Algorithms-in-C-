750c89294088a87da6841ed0a105c94ae7e8220a
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-10-02 19:25:58 +0200

    create hello.cs file to print statment hello world

---
+++ hello.cs
@@ -0,0 +1,15 @@
+// create C# program to print hello world
+using System;
+namespace HelloWorldApp{
+	class SayHello{
+		// Main Method 
+		static void Main( String[] args){
+			//print the statment
+			Console.WriteLine("Hello World");
+			 // To prevents the screen from  
+            // running and closing quickly 
+            Console.ReadKey();
+			}
+
+		}
+	}
